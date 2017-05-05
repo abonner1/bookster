bd4c0c5e3c648dba110d99e4610700e85f9b28e8
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-05 11:36:24 -0400

    Initial commit

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,12 @@
+require "./config/environment"
+
+class ApplicationController < Sinatra::Base
+
+  configure do
+    set :public_folder, "public"
+    set :views, "app/views"
+    enable :sessions
+    get :session_secret, "bloggerama"
+  end
+
+end
