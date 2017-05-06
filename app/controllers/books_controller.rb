e7db7442570891549b6c1c4dafa7ef7e20cb237d
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-06 13:09:12 -0400

    add edit route

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,23 +12,26 @@ class BooksController < ApplicationController
   end
 
   post '/books' do
-    @book = Book.create(params)
+    @book = Book.create(params[:book])
     redirect to "/books/#{book.id}"
   end
 
   # show action
   get '/books/:id' do
     @book = Book.find_by_id(params[:id])
-    erb :'/books/show'
+    erb :'/books/show_book'
   end
 
   # edit action
   get '/books/:id/edit' do
-
+    @book = Book.find_by_id(params[:id])
+    erb :'/books/edit_book'
   end
 
   patch '/books/:id' do
-
+    @book = Book.find_by_id(params[:id])
+    @book.update(params[:book])
+    redirect to "/books/#{@book.id}"
   end
 
   # delete action
