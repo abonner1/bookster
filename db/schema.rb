59c012362e95985b9982a913206a8acbe7d4a7a1
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-06 13:17:18 -0400

    add copyright field

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170506133202) do
+ActiveRecord::Schema.define(version: 20170506171345) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170506133202) do
     t.float "price"
     t.integer "availability"
     t.integer "user_id"
+    t.string "copyright"
   end
 
   create_table "users", force: :cascade do |t|
