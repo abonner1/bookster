d815c0a0c8bf1a69328a80fdcfd63733fcdaf36c
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-06 09:56:29 -0400

    add slugifiable tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,15 @@ describe 'User' do
     @user = User.create(username: "alice 12b", email: "alice12b@gmail.com", password: "alice21")
   end
 
+  it "can slug the username" do
+    expect(@user.slug).to eq("alice-12b")
+  end
+
+  it "can find a user based on the slug" do
+    slug = @user.slug
+    expect(User.find_by_slug(slug).username).to eq("alice 12b")
+  end
+
   it 'has a secure password' do
     expect(@user.authenticate("book")).to eq(false)
     expect(@user.authenticate("alice21")).to eq(@user)
