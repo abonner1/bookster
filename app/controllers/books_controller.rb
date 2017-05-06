f8e8aa2d8e83d84278a21d37cdcd1f49da56a7e3
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-06 12:35:56 -0400

    stub out routes

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,37 @@
 class BooksController < ApplicationController
 
+  # index action
   get '/books' do
     @books = Book.all
     erb :'/books/index'
+  end
+
+  # new action
+  get '/books/new' do
+
+  end
+
+  post '/books' do
+
+  end
+
+  # show action
+  get '/books/:id' do
+
+  end
+
+  # edit action
+  get '/books/:id/edit' do
+
+  end
+
+  patch '/books/:id' do
+
+  end
+
+  # delete action
+  delete '/books/:id/delete' do
+    
   end
 
 end
