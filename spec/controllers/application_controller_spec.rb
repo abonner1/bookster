f72c1e6e94c8e089d4e318e387bbf4c12419f16c
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-06 10:12:17 -0400

    add homepage test

---
+++ spec/controllers/application_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe "ApplicationController" do
+
+  describe "Homepage" do
+    it 'loads the homepage' do
+      get '/'
+      expect(last_response.status).to eq(200)
+      expect(last_response.body).to include("Welcome to Andy and Ross's Bookshop")
+    end
+  end
+
+end
