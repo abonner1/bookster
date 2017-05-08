b493a5ac5f6f8632fcdd280404c2e5bb05f6661b
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-08 19:17:26 -0400

    add signup route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+
+  get '/signup' do
+    if logged_in?
+      redirect to '/books'
+    else
+      erb :'/users/signup'
+    end
+  end
 end
--- config.ru
+++ config.ru
@@ -6,6 +6,7 @@ end
 
 use Rack::MethodOverride
 
+use UsersController
 use BooksController
 
 run ApplicationController
