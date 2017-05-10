2e485b01e7b0cdb61d92d0e596a8b808c798adf3
Andrew Bonner <andrewbonner92@gmail.com>
2017-05-10 11:53:32 -0400

    add validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,5 @@
 class Book < ActiveRecord::Base
   belongs_to :user
+
+  validates :title, :author, :price, :availability, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ActiveRecord::Base
   has_many :books
   has_secure_password
 
+  validates :username, :email, :password, presence: true
+
   extend Slugifiable::ClassMethods
   include Slugifiable::InstanceMethods
 end
